b2305ed86b8f9894cd464322acf599bcc8073f63
Kushnu14 <152047235+Kushnu14@users.noreply.github.com>
2024-11-24 10:00:37 +0530

    Updated MainScreen.java

--- app/src/main/java/com/example/hostelfinder/MainScreen.java
+++ app/src/main/java/com/example/hostelfinder/MainScreen.java
@@ -16,7 +16,6 @@ import com.example.hostelfinder.databinding.ActivityMainScreenBinding;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-
 import java.util.ArrayList;
 import java.util.Objects;
 
